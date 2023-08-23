c8a87e18c39c97a757e987271269027e3c736dbd
sandralopez <slopez52751@gmail.com>
2023-08-23 13:25:01 +0200

    Longer footer line.

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
 	return (
 		<div className="flex justify-center items-center mt-20 mb-10 w-full text-gray-500 dark:text-slate-400">
-			<div className="w-9/12">
+			<div className="w-10/12">
 				<div className="flex justify-between mb-2">
 					<p>Pet Log</p>
 					<div className="flex">
